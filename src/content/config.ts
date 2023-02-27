febdddcca154737a875f59475e691b005dd0e68c
nojaf <florian.verdonck@outlook.com>
2023-02-27 12:00:05 +0100

    Initial session pages.

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,8 +10,23 @@ const testimonialCollection = defineCollection({
     profilePicture: z.string(),
   }),
 });
+
+const sessionCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    preview: z.string(),
+    isDraft: z.boolean(),
+    date: z.date().transform((str) => new Date(str)),
+    champion: z.string(),
+    company: z.string().optional(),
+    repository: z.string().optional(),
+    issueLink: z.string().optional(),
+  }),
+});
+
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
   testimonials: testimonialCollection,
+  sessions: sessionCollection,
 };
